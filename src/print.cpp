97e72fc9efa85db992e2f13be2681f2d18f4d479
Andrew Dornbush <andrew.dornbush@gmail.com>
2014-10-13 14:28:05 -0400

    more pretty migration

--- src/print.cpp
+++ src/print.cpp
@@ -2,26 +2,26 @@
 
 void leatherman::printPoseMsg(const geometry_msgs::Pose &p, std::string text)
 {
-  ROS_INFO("[%s] xyz: %0.3f %0.3f %0.3f  quat: %0.3f %0.3f %0.3f %0.3f", text.c_str(), p.position.x, p.position.y, p.position.z, p.orientation.x, p.orientation.y, p.orientation.z, p.orientation.w);
+  ROS_INFO_PRETTY("[%s] xyz: %0.3f %0.3f %0.3f  quat: %0.3f %0.3f %0.3f %0.3f", text.c_str(), p.position.x, p.position.y, p.position.z, p.orientation.x, p.orientation.y, p.orientation.z, p.orientation.w);
 }
 
 void leatherman::printPoseStampedMsg(const geometry_msgs::PoseStamped &p, std::string text)
 {
-  ROS_INFO("[%s] xyz: %0.3f %0.3f %0.3f  quat: %0.3f %0.3f %0.3f %0.3f  time: % 0.3f", text.c_str(), p.pose.position.x, p.pose.position.y, p.pose.position.z, p.pose.orientation.x, p.pose.orientation.y, p.pose.orientation.z, p.pose.orientation.w, p.header.stamp.toSec());
+  ROS_INFO_PRETTY("[%s] xyz: %0.3f %0.3f %0.3f  quat: %0.3f %0.3f %0.3f %0.3f  time: % 0.3f", text.c_str(), p.pose.position.x, p.pose.position.y, p.pose.position.z, p.pose.orientation.x, p.pose.orientation.y, p.pose.orientation.z, p.pose.orientation.w, p.header.stamp.toSec());
 }
 
 void leatherman::printPose(const std::vector<double> &p, std::string text)
 {
-  ROS_INFO("[%s] xyz: % 0.3f % 0.3f % 0.3f  rpy: % 0.3f % 0.3f % 0.3f", text.c_str(), p[0], p[1], p[2], p[3], p[4], p[5]);
+  ROS_INFO_PRETTY("[%s] xyz: % 0.3f % 0.3f % 0.3f  rpy: % 0.3f % 0.3f % 0.3f", text.c_str(), p[0], p[1], p[2], p[3], p[4], p[5]);
 }
 
 void leatherman::printJointTrajectory(const trajectory_msgs::JointTrajectory &traj, std::string text)
 {
   if(!traj.points.empty())
-    ROS_INFO("[%s] start_time: %0.3f", text.c_str(), traj.header.stamp.toSec());
+    ROS_INFO_PRETTY("[%s] start_time: %0.3f", text.c_str(), traj.header.stamp.toSec());
   for(unsigned int i = 0; i < traj.points.size(); i++)
   {
-    ROS_INFO("[%s] [% 2d] % 1.4f % 1.4f % 1.4f % 1.4f % 1.4f % 1.4f % 1.4f   time_from_start: % 0.3f", text.c_str(), i,traj.points[i].positions[0],traj.points[i].positions[1],traj.points[i].positions[2],traj.points[i].positions[3],traj.points[i].positions[4],traj.points[i].positions[5],traj.points[i].positions[6], traj.points[i].time_from_start.toSec());
+    ROS_INFO_PRETTY("[%s] [% 2d] % 1.4f % 1.4f % 1.4f % 1.4f % 1.4f % 1.4f % 1.4f   time_from_start: % 0.3f", text.c_str(), i,traj.points[i].positions[0],traj.points[i].positions[1],traj.points[i].positions[2],traj.points[i].positions[3],traj.points[i].positions[4],traj.points[i].positions[5],traj.points[i].positions[6], traj.points[i].time_from_start.toSec());
   }
 }
 
@@ -29,7 +29,7 @@ void leatherman::printJointTrajectoryPoints(const std::vector<trajectory_msgs::J
 {
   for(unsigned int i = 0; i < points.size(); i++)
   {
-    ROS_INFO("[%s] [% 2d] % 1.4f % 1.4f % 1.4f % 1.4f % 1.4f % 1.4f % 1.4f   time_from_start: % 0.3f", text.c_str(), i,points[i].positions[0],points[i].positions[1],points[i].positions[2],points[i].positions[3],points[i].positions[4],points[i].positions[5],points[i].positions[6], points[i].time_from_start.toSec());
+    ROS_INFO_PRETTY("[%s] [% 2d] % 1.4f % 1.4f % 1.4f % 1.4f % 1.4f % 1.4f % 1.4f   time_from_start: % 0.3f", text.c_str(), i,points[i].positions[0],points[i].positions[1],points[i].positions[2],points[i].positions[3],points[i].positions[4],points[i].positions[5],points[i].positions[6], points[i].time_from_start.toSec());
   }
 }
 void leatherman::printCompleteJointTrajectory(const trajectory_msgs::JointTrajectory &traj, std::string name)
@@ -66,13 +66,13 @@ void leatherman::printCompleteJointTrajectory(const trajectory_msgs::JointTrajec
 
 void leatherman::printAffine3d(const Eigen::Affine3d &a, std::string text)
 {
-  ROS_INFO("[%s] xyz: %0.3f %0.3f %0.3f", text.c_str(), a.translation().x(), a.translation().y(), a.translation().z());
+  ROS_INFO_PRETTY("[%s] xyz: %0.3f %0.3f %0.3f", text.c_str(), a.translation().x(), a.translation().y(), a.translation().z());
 }
 
 void leatherman::printKDLFrame(const KDL::Frame &f, std::string text)
 {
   double r,p,y; f.M.GetRPY(r,p,y);
-  ROS_INFO("[%s] xyz: %0.3f %0.3f %0.3f  rpy: %0.3f %0.3f %0.3f", text.c_str(), f.p[0], f.p[1], f.p[2], r, p, y);
+  ROS_INFO_PRETTY("[%s] xyz: %0.3f %0.3f %0.3f  rpy: %0.3f %0.3f %0.3f", text.c_str(), f.p[0], f.p[1], f.p[2], r, p, y);
 }
 
 void leatherman::printKDLFrames(const std::vector<std::vector<KDL::Frame> > &f, std::string text)
@@ -83,7 +83,7 @@ void leatherman::printKDLFrames(const std::vector<std::vector<KDL::Frame> > &f, 
     for(size_t j = 0; j < f[i].size(); ++j)
     {
       f[i][j].M.GetRPY(r,p,y);
-      ROS_INFO("[%s] [%d %d]  xyz: %0.3f %0.3f %0.3f  rpy: %0.3f %0.3f %0.3f", text.c_str(), int(i), int(j), f[i][j].p.x(), f[i][j].p.y(), f[i][j].p.z(), r, p, y);
+      ROS_INFO_PRETTY("[%s] [%d %d]  xyz: %0.3f %0.3f %0.3f  rpy: %0.3f %0.3f %0.3f", text.c_str(), int(i), int(j), f[i][j].p.x(), f[i][j].p.y(), f[i][j].p.z(), r, p, y);
     }
   }
 }
@@ -91,12 +91,12 @@ void leatherman::printKDLFrames(const std::vector<std::vector<KDL::Frame> > &f, 
 
 void leatherman::printKDLChain(const KDL::Chain &c, std::string text)
 {
-  ROS_INFO("[%s] # segments: %d  # joints: %d", text.c_str(), c.getNrOfSegments(), c.getNrOfJoints());
+  ROS_INFO_PRETTY("[%s] # segments: %d  # joints: %d", text.c_str(), c.getNrOfSegments(), c.getNrOfJoints());
   double r,p,y,jnt_pos = 0;
   for(size_t j = 0; j < c.getNrOfSegments(); ++j)
   {
     c.getSegment(j).pose(jnt_pos).M.GetRPY(r,p,y);
-    ROS_INFO("[%s] [%2d] segment: %21s  xyz: %0.3f %0.3f %0.3f  rpy: %0.3f %0.3f %0.3f", 
+    ROS_INFO_PRETTY("[%s] [%2d] segment: %21s  xyz: %0.3f %0.3f %0.3f  rpy: %0.3f %0.3f %0.3f", 
         text.c_str(), int(j),
         c.getSegment(j).getName().c_str(),
         c.getSegment(j).pose(jnt_pos).p.x(),
@@ -105,7 +105,7 @@ void leatherman::printKDLChain(const KDL::Chain &c, std::string text)
         r,p,y);
 
     c.getSegment(j).getJoint().pose(jnt_pos).M.GetRPY(r,p,y);
-    ROS_INFO("[%s]        joint: %21s  xyz: %0.3f %0.3f %0.3f  rpy: %0.3f %0.3f %0.3f  type: %s", 
+    ROS_INFO_PRETTY("[%s]        joint: %21s  xyz: %0.3f %0.3f %0.3f  rpy: %0.3f %0.3f %0.3f  type: %s", 
         text.c_str(),
         c.getSegment(j).getJoint().getName().c_str(),
         c.getSegment(j).getJoint().pose(jnt_pos).p.x(),
